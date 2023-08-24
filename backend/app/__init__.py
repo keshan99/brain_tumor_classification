3fdb7230d0d7b73212d3228053e0fa7e7176704e
keshan_mwm <mwmadushakeshan99@gmail.com>
2023-08-24 13:46:18 +0530

    change db2

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 import os
@@ -10,7 +9,10 @@ db = MySQL()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+    app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+    app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
     
     db.init_app(app)
     
